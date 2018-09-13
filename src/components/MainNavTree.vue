74d008069ab80a7563adc18f228d60c7c2c37ef6
adam <adamytd@gmail.com>
2018-09-13 12:05:32 +0800

    [Add] main nav tree component

---
+++ src/components/MainNavTree.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="nav-tree">
+    <el-tree :data="treeData" node-key="id" :default-expanded-keys="[0,1]" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'NavTree',
+  data() {
+    return {
+      treeData: [{
+        id: 0,
+        label: '系统',
+        children: [{
+          id: 1,
+          label: '物料信息管理',
+          // children: [{
+          //   label: '三级 1-1-1'
+          // }]
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      },
+
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+      if(data.label === "物料信息管理"){
+        this.$router.push('/');
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.nav-tree {
+  width: 200px;
+}
+</style>
